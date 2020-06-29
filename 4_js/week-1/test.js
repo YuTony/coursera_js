6ae35f45a63778c650448acd791fe5a886ddf07b
YuTony <versities@gmail.com>
2020-06-29 17:19:31 +0300

    start course 4

---
+++ 4_js/week-1/test.js
@@ -0,0 +1,40 @@
+var obj = {}
+var students = [];
+console.log(Object.getPrototypeOf(students));
+console.log();
+
+var sleep = function () {};
+console.log(Object.getPrototypeOf(sleep));
+console.log();
+
+var person = {
+    type: 'person'
+}
+var danny = {
+    name: 'Danny',
+    type: undefined
+}
+Object.setPrototypeOf(danny, person);
+
+var student = {};
+Object.prototype.serialize = function () {};
+
+console.log('serialize' in student);
+console.log('serialize' in []);
+console.log('serialize' in new Date());
+console.log('serialize' in Object);
+console.log('serialize' in Object.prototype); 
+Object.prototype.serialize = function () {};
+var student = {
+  name: 'Ivan',
+  getName: function () { return this.name; }
+};
+
+Object.defineProperty(student, 'type', {
+  enumerable: false,
+  value: 'student'
+});
+
+for (var key in student) {
+  console.log(key);
+}
